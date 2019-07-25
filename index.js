4ac680b8beeb8e30801a1bf89375924eeb9ade8c
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-07-25 10:46:13 +0430

    index.js added as main page

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const router = require('./lib/router/router').router
+
+const server = http.createServer()
+
+
+
+server.on('request', function (request, response) {
+  router.lookup(request, response)
+
+})
+
+server.listen(3002, function () {
+  console.log(`server is running on port 3002`)
+})
