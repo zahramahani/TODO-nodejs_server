363e80bb2d0a1ef421301605719af50dbbc21e11
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-09-01 10:19:35 +0430

    npm i runed

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ server.on('request', function (request, response) {
   router.lookup(request, response)
 })
 
-server.listen(6000, function () {
-  console.log(`server is running on port 6000`)
+server.listen(3001, function () {
+  console.log(`server is running on port 3001`)
 })
