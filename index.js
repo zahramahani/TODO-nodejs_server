db1f82d87ce77c6fe500e5233e8ace38f9878a38
Sara Khorashadi <sara.khorashadi@partdp.ir>
2019-09-02 11:16:17 +0430

    client replaced

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ server.on('request', function (request, response) {
   router.lookup(request, response)
 })
 
-server.listen(3004, function () {
-  console.log(`server is running on port 3004`)
+server.listen(6000, function () {
+  console.log(`server is running on port 6000`)
 })
