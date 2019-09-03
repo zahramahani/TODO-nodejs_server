736b42b590e6ce8fcf53ccdd6c4f1b795cde38cb
Sara Khorashadi <sara.khorashadi@partdp.ir>
2019-09-03 12:16:08 +0430

    server definition and used took apart

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const http = require('http')
-const router = require('./lib/router/router').router
-
-const server = http.createServer()
-
-server.on('request', function (request, response) {
-  router.lookup(request, response)
-})
-
-server.listen(6000, function () {
+require('dotenv').config()
+const server = require('./lib/server').server
+const PORT = process.env.PORT || 80
+server.listen(PORT, function () {
   console.log(`server is running on port 6000`)
 })
